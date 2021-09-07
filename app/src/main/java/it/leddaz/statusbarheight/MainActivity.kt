315e0ae4b77a2eff85be3a8e64d263223c46bb72
LeddaZ <leonardoledda@gmail.com>
2021-09-07 18:35:13 +0200

    Initial commit

---
+++ app/src/main/java/it/leddaz/statusbarheight/MainActivity.kt
@@ -0,0 +1,29 @@
+package it.leddaz.statusbarheight
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        display()
+    }
+
+    fun getStatusBarHeight(): Int {
+        var result = 0
+        val resourceId = resources.getIdentifier("status_bar_height", "dimen", "android")
+        if (resourceId > 0) {
+            result = resources.getDimensionPixelSize(resourceId)
+        }
+        return result
+    }
+
+    private fun display() {
+        val height = findViewById<TextView>(R.id.heightStr)
+        height.text = getStatusBarHeight().toString() + "px"
+    }
+}
